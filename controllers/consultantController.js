bfe267bf92b068536a4a75ba280be90e966c323e
hasiniV19 <83814896+hasiniV19@users.noreply.github.com>
2022-09-27 16:44:44 +0530

    set deadline

--- controllers/consultantController.js
+++ controllers/consultantController.js
@@ -8,3 +8,22 @@ const createSchedule = async (req, res) => {
         res.send(body); //Display the response on the website
       }); 
 }
+
+const setDeadline = async (req, res) => {
+  console.log("got data")
+  const data = req.body
+
+  console.log(data)
+  // add doc to database
+  // try {
+  //     const workout = await Workout.create({title, load, reps})
+  //     res.status(200).json(workout)
+  // } catch (error) {
+  //     res.status(400).json({error: error.message})
+  // }
+  // res.json({msg: 'POST a new workout'})
+}
+
+module.exports = {
+  setDeadline
+}
--- routes/api/consultant.js
+++ routes/api/consultant.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const {
-
+    setDeadline
 } = require('../../controllers/consultantController')
 
 const router = express.Router()
 
 router
     .post('/create-schedule')
+    .post('/set-deadline', setDeadline)
 
 
 
