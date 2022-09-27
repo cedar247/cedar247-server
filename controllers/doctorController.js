7cef47a3ff19343ba2fd1d1e9abbccf497c85e7c
ShihanVidulanka <81677102+ShihanVidulanka@users.noreply.github.com>
2022-09-27 23:40:44 +0530

    define requirements connect front end with backend

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,104 +1,18 @@
-const Doctor = require("../models/doctorModel");
-const mongoose = require("mongoose");
+const defineRequirements = async (req, res) => {
+    console.log("got data");
+    const data = req.body;
 
-// get all doctor
-const getDoctors = async (req, res) => {
-    const doctors = await Doctor.find({}).sort({ createdAt: -1 });
-
-    res.status(200).json(doctors);
-};
-
-// get a single doctor
-const getDoctor = async (req, res) => {
-    const { id } = req.params;
-
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ error: "No such doctor" });
-    }
-
-    const doctor = await Doctor.findById(id);
-
-    if (!doctor) {
-        return res.status(404).json({ error: "No such doctor" });
-    }
-
-    res.status(200).json(doctor);
-};
-
-// create a new doctor
-const createDoctor = async (req, res) => {
-    const { name, phoneNumber, email, category, WardID } = req.body;
-
-    let emptyFields = [];
-
-    if (!name) {
-        emptyFields.push("name");
-    }
-    if (!phoneNumber) {
-        emptyFields.push("phoneNumber");
-    }
-    if (!email) {
-        emptyFields.push("email");
-    }
-    if (!category) {
-        emptyFields.push("category");
-    }
-    if (!WardID) {
-        emptyFields.push("WardID");
-    }
-    if (emptyFields.length > 0) {
-        return res
-            .status(400)
-            .json({ error: "Please fill in all fields", emptyFields });
-    }
-
-    // add to the database
-    try {
-        const doctor = await Doctor.create({ name, phoneNumber, email, category, WardID });
-        res.status(200).json(doctor);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-};
-
-// delete a doctor
-const deleteDoctor = async (req, res) => {
-    const { id } = req.params;
-
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(400).json({ error: "No such doctor" });
-    }
-
-    const doctor = await Doctor.findOneAndDelete({ _id: id });
-
-    if (!doctor) {
-        return res.status(400).json({ error: "No such doctor" });
-    }
-
-    res.status(200).json(doctor);
-};
-
-// update a doctor
-const updateDoctor = async (req, res) => {
-    const { id } = req.params;
-
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(400).json({ error: "No such doctor" });
-    }
-
-    const doctor = await Doctor.findOneAndUpdate({ _id: id },{...req.body,});
-
-    if (!doctor) {
-        return res.status(400).json({ error: "No such doctor" });
-    }
-
-    res.status(200).json(doctor);
+    console.log(data);
+    // add doc to database
+    // try {
+    //     const workout = await Workout.create({title, load, reps})
+    //     res.status(200).json(workout)
+    // } catch (error) {
+    //     res.status(400).json({error: error.message})
+    // }
+    // res.json({msg: 'POST a new workout'})
 };
 
 module.exports = {
-    getDoctors,
-    getDoctor,
-    createDoctor,
-    deleteDoctor,
-    updateDoctor,
+    defineRequirements,
 };
--- routes/api/doctor.js
+++ routes/api/doctor.js
@@ -1,28 +1,13 @@
 const express = require('express')
 const {
-    getDoctors,
-    getDoctor,
-    createDoctor,
-    deleteDoctor,
-    updateDoctor,
+    defineRequirements
 } = require('../../controllers/doctorController')
 
 const router = express.Router()
 
-// GET all workouts
-router.get("/", getDoctors);
+router
+    .post('/defineRequirements',defineRequirements)
 
-// GET a single workout
-router.get("/:id", getDoctor);
-
-// POST a new workout
-router.post("/", createDoctor);
-
-// DELETE a workout
-router.delete("/:id", deleteDoctor);
-
-// UPDATE a workout
-router.patch("/:id", updateDoctor);
 
 
 module.exports = router
