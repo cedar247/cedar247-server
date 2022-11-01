43f5001c9994186dd32491bd0f0a2d1e7c765a85
Vinojithab17 <gunaratne.19@cse.mrt.ac.lk>
2022-11-01 20:46:57 +0530

    Auththentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,8 +8,8 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt');
 
 // function to create the token
-const createToken = (_id) =>{
-    return jwt.sign({_id},process.env.SECRET,{expiresIn:'2d'})
+const createToken = (_id,type) =>{
+    return jwt.sign({_id,type},process.env.SECRET,{expiresIn:'2d'})
 }
 // function to do a user login
 const doLogin = async (req,res)=>{
@@ -24,8 +24,8 @@ const doLogin = async (req,res)=>{
         //if user exits the checks the password 
         if(bcrypt.compareSync(req.body.password, user.password)){
             //if password is correct then creats a jwt token
-            const token = createToken(user._id)
-            console.log(user._id);
+            const token = createToken(user._id,user.type)
+            console.log(user._id,user.type);
             // return the token and other details
         return res.status(200).json({status:"ok", userid : user,token:token})
         }
